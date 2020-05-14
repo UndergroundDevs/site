aefca98759679400216bd184c578255f2ef0a88d
KevsonDoc <filipemorais576@gmail.com>
2020-05-14 10:11:03 -0300

    [update] - Contact full backend

--- app.js
+++ app.js
@@ -38,12 +38,67 @@ app.get('/about/wanghley-martins', function(req, res) {
   app.get('/services',function (req,res){
     res.sendFile(__dirname+'/static/services/index.html')
 });
-/*==============================
+/*==============================================================================
             CONTACT
-  ==============================*/
+  ============================================================================*/
+  // body parser middleware
+app.use(express.json());
+app.use(express.urlencoded( { extended: false } )); // this is to handle URL encoded data
+// end parser middleware
+
+
+// custom middleware to log data access
+const log = function (request, response, next) {
+	console.log(`${new Date()}: ${request.protocol}://${request.get('host')}${request.originalUrl}`);
+	console.log(request.body); // make sure JSON middleware is loaded before this line
+	next();
+}
+app.use(log);
+// end custom middleware
+
+// enable static files pointing to the folder "public"
+// this can be used to serve the index.html file
   app.get('/contact',function (req,res){
     res.sendFile(__dirname+'/static/contact/index.html')
 });
+
+// HTTP POST
+app.post("/ajax/email", function(request, response) { // this will be used to send the emails
+  const trasposter = nodemailer.createTransport({
+      host: "smtp.gmail.com",
+      port: 465,
+      secure: true,
+      auth: {
+          user: "underdevs.community@gmail.com",
+          pass: "underground2020",
+      }
+  });
+
+var textBody = `FROM: ${request.body.name} EMAIL: ${request.body.email} MESSAGE: ${request.body.message}`;
+var htmlBody = `<h2>Underground DEVs</h2><p>from: ${request.body.name} <a href="mailto:${request.body.email}">${request.body.email}</a></p><p>${request.body.message}</p>`;
+var mail = {
+  from: 'fiipes278@gmail.com', // sender address
+  to: 'underdevs.community@gmail.com, filipemorais576@gmail.com, Kardecallan56@gmail.com, wanghleys@gmail.com, pablomirandarocha11@gmail.com, ekilobin@gmail.com', // list of receivers (THIS COULD BE A DIFFERENT ADDRESS or ADDRESSES SEPARATED BY COMMAS)
+  subject: "Underground DEVs", // Subject line
+  text: textBody,
+  html: htmlBody
+  };
+
+  // send mail with defined transport object
+trasposter.sendMail(mail, function (err, info) {
+  if(err) {
+    console.log(err);
+    response.json({ message: "message not sent: error" });
+  }
+  else {
+    response.json({ message: `Email emviado` });
+  }
+});
+});
+/*==============================================================================
+          FIM DE CONTACT
+  ============================================================================*/
+
 
 /*==============================
             Projects
@@ -83,13 +138,6 @@ app.use('/contact/css', express.static(__dirname + '/static/contact/css'));
 app.use((req, res, next) => {
     res.status(404).redirect('/error/404')
   });
-  
-
-
-
-
-
-
 
 
 
